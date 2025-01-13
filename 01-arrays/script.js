c64681a35cc169e9fd024957fabd419334a41a5b
IgorGoro91 <igorysc@gmail.com>
2025-01-13 16:22:35 +0100

    add:arrays

---
+++ 01-arrays/script.js
@@ -0,0 +1,52 @@
+const teachers = [
+  'Nathan',
+  'Ed',
+  'Fabio',
+  'Phil',
+  'Carlo',
+  'Lewis',
+  'Luca'
+]; // NON MODIFICARE QUESTA VARIABILE
+
+
+// 1. Utilizzando un indice sull'array teachers, cambia il valore della
+// variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
+const fourthTeacher = null;
+
+// 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+console.log(teachers, teachers.length)
+teachers.splice(5, 1, "Patrick" ) 
+console.log(teachers, teachers.length)
+
+// 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
+console.log(teachers, teachers.length)
+const lastTeacher = teachers.pop();
+console.log(teachers, teachers.length)
+
+// 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
+console.log(teachers, teachers.length)
+const firstTeacher = teachers.shift();
+console.log(teachers, teachers.length)
+
+// 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+
+console.log(teachers, teachers.length)
+teachers.push("Vanessa")
+console.log(teachers, teachers.length)
+
+
+
+// 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+
+console.log(teachers, teachers.length)
+teachers.unshift("Sarah")
+console.log(teachers, teachers.length)
+
+
+// 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
+// e salvalo nella variabile lewisIndex
+const lewisIndex =null;
+
+// 8. Verifica se l'array teachers è vuoto
+// e salva il risultato nella variabile isTeachersEmpty
+const isTeachersEmpty = null;
